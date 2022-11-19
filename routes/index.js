2cd8f253a0536acfbf4bae18b27497f37f32bc44
Agiel <fadilahagiel@gmail.com>
2022-11-19 15:32:07 +0700

    show, add, delete wishlist

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router()
 const userRouter = require('./userRouter')
 const productRouter = require('./productRouter')
 const categoriesRouter = require('./categoryRouter')
+const wishlistRouter = require('./wishlistRouter')
 const authentication = require('../middlewares/authentication')
 
 router.use('/users', userRouter)
 router.use(authentication)
 router.use('/products', productRouter)
 router.use('/categories', categoriesRouter)
+router.use('/wishlists', wishlistRouter)
 
 module.exports = router
